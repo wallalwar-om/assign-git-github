f31664e98f83c48e7bae537a80db4251f180e9d5
omwallalwar-dev <githubdevelopom0196@gmail.com>
2026-02-07 15:00:10 +0530

    Keep only Item ID field in To-Do form

--- app.py
+++ app.py
@@ -6,10 +6,14 @@ import json
 
 load_dotenv()
 app = Flask(__name__)
+
 MONGO_URL = os.getenv("MONGO_URL")
 client = MongoClient(MONGO_URL)
 db = client['user_database']
+
 users_collection = db['users']
+todo_collection = db['todo_items']
+
 
 @app.route('/')
 def index():
@@ -26,25 +30,38 @@ def submit():
         }
         users_collection.insert_one(data)
         return redirect(url_for('success'))
-    
     except Exception:
         return render_template('form.html', error="An error occurred while submitting the data. Please try again.")
-    
+
 
 @app.route('/success')
 def success():
     return "Data submitted successfully!"
 
-@app.route('/api')
-def api():
-    with open('data.json', 'r') as f:
-        data = json.load(f)
-    return data
 
 @app.route('/todo')
 def todo():
     return render_template('todo.html')
 
+
+@app.route('/submittodoitem', methods=['POST'])
+def submit_todo_item():
+    try:
+        item = {
+            "itemName": request.form['itemName'],
+            "itemDescription": request.form['itemDescription']
+        }
+        todo_collection.insert_one(item)
+        return "To-Do item added successfully!"
+    except Exception:
+        return "Error while adding To-Do item", 500
+
+
+@app.route('/api')
+def api():
+    with open('data.json') as f:
+        data = json.load(f)
+    return data
 
 
 if __name__ == '__main__':
